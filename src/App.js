cc0fe86fe7bb9ad3fc79e029f574bfe80e13d9c6
just-anto <antomontagna6@gmail.com>
2022-10-24 16:46:52 -0300

    particles css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Particles from "./app/components/Particles";
+
+function App(){
+    return (
+<>
+<Particles/>
+</>
+    );
+}
+
+export default App;
